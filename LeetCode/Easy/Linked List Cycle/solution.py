724d2646fc19c1b7d54649857aba0e6fc63fc9c5
24f2006845 <Haiderkg78@gmail.com>
2026-09-03 18:58:30 +0530

    Refactor hasCycle method to use Floyd's algorithm
    Refactored hasCycle method to use Floyd's algorithm with slow and fast pointers instead of brute force with a visited list.

--- LeetCode/Easy/Linked List Cycle/solution.py
+++ LeetCode/Easy/Linked List Cycle/solution.py
@@ -3,16 +3,27 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
-class Solution:
+# solution 1
+# brute force
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         current = head
+#         visited = []
+#         while current is not None:
+#             if current in visited:
+#                 return True
+#             visited.append(current)
+#             current = current.next
+#         return False
+# Floyd algo also called slow and fast pointer
+ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        current = head
-        visited = []
-        while current is not None:
-            if current in visited:
+        fast = head
+        slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next 
+            if slow == fast :
                 return True
-            visited.append(current)
-            current = current.next
         return False
-
         
