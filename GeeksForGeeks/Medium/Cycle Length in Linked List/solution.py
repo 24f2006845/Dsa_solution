70c7290f8aa1417384214bc6a91f7c9943be4d74
24f2006845 <Haiderkg78@gmail.com>
2026-09-03 21:43:23 +0530

    Improve cycle length calculation in linked list
    Refactor cycle length detection logic in lengthOfLoop method.

--- GeeksForGeeks/Medium/Cycle Length in Linked List/solution.py
+++ GeeksForGeeks/Medium/Cycle Length in Linked List/solution.py
@@ -4,27 +4,44 @@ class Node:
         self.data = data
         self.next = None
 '''
+# class Solution:
+#     def lengthOfLoop(self, head):
+#         #code here
+#         slow = head
+#         fast = head
+
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+
+#             if slow == fast:
+#                 # cycle detected
+
+#                 count = 1
+#                 current = slow.next
+
+#                 while current != slow:
+#                     count += 1
+#                     current = current.next
+
+#                 return count
+
+#         return 0
+
 class Solution:
     def lengthOfLoop(self, head):
         #code here
         slow = head
         fast = head
-
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-
             if slow == fast:
-                # cycle detected
-
+                slow = slow.next
                 count = 1
-                current = slow.next
-
-                while current != slow:
-                    count += 1
-                    current = current.next
-
+                while slow != fast:
+                    slow = slow.next
+                    count+=1
                 return count
-
         return 0
         
