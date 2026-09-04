6b73aa5a325e150062c4b4894f4f638479e4afee
24f2006845 <Haiderkg78@gmail.com>
2026-09-04 18:57:58 +0530

    Implement two solutions for removing Nth node
    Added a second solution using a one-pass approach to remove the Nth node from the end of the list.

--- LeetCode/Medium/Remove Nth Node From End of List/solution.py
+++ LeetCode/Medium/Remove Nth Node From End of List/solution.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+## solution 1 using two pass 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         current = head 
@@ -23,5 +24,21 @@ class Solution:
             current = current.next
         prev.next = current.next
         return head
+
+## solution 2  by one pass 
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        for _ in range(n):
+            fast = fast.next
+        if fast == None:
+            return head.next
+        while fast.next is not None:
+            slow = slow.next
+            fast = fast.next
+        slow.next = slow.next.next
+        return head
+# both have same complexity just i write solution2 bcz leetcode says that
         
         
